c47e05a0387a3175779ab2a5f2c16326c8930c1b
Neokyuubi <40678329+neokyuubi@users.noreply.github.com>
2023-05-16 03:38:20 +0200

    added 2 components

---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Message extends Component
+{
+    render()
+    {
+        return <h1>this is a class component Message</h1>;
+    };
+}
+
+export default Message;
